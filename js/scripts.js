62006464ab18b0cdd3ba9fe05f92ba328d2bf065
Austin Lords <austinlords@outlook.com>
2019-04-01 11:43:53 -0700

    project is functional. Minimal styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,30 @@
 //back-end logic
 
-var allNumbers = [];
-var primeNumbers = [];
-var userNumber;
-
-var findPrime = function(number) {
-
+function findPrime(number) {
+  var numbers = [];
+  for (i=2; i<number; i++) {
+    numbers.push(i);
+  }
+  numbers.forEach(function(element1) {
+    numbers.forEach(function(element2) {
+      if (element2 > element1 && element2 % element1 === 0) {
+        i = numbers.indexOf(element2);
+        numbers.splice(i,1);
+      }
+    })
+  });
+  return numbers
 }
 
 //front-end logic
 $(function() {
-  $("#primeNumbersFinder").submit(function(event) {
+  $("#primeForm").submit(function(event) {
     event.preventDefault();
-    userNumber = parseInt($("#userNumberInput").val());
-    var i;
-    for (i = 2; i <= userNumber; ++i) {
-      allNumbers.push(i);
-    }
-    allNumbers.forEach(function(number) {
-      var smallestMultiple = number;
-      allNumbers.forEach(function(number2) {
-        console.log(number2/smallestMultiple);
-        if ((number2%smallestMultiple) === 0) {
-          allNumbers.splice(allNumbers.indexOf(number2 - 2), 1)
-          console.log(allNumbers);
-        } //else console.log("didn't work");
-      });
-    });
-    $("#primeNumbers").html(allNumbers);
+    var userNumber = parseInt($("#userInput").val());
+    var primeList = findPrime(userNumber);
+    primeStr = primeList.join(', ');
+    $("#primeNumbers").html(primeStr);
     $(".userNumber").html(userNumber);
     $(".hidden").show();
-  }); //end of submit function
-}); //end of front-end logic
+  }); 
+});
